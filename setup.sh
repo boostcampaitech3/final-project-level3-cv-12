948ed559918c0339cb85189839f97b17b6623b1e
ChoiHongrok <zxcv5317@naver.com>
2022-04-14 13:40:21 +0000

    update setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+conda install pytorch=1.7 cudatoolkit=11.0 torchcision=0.8.0 -c pytorch
+pip install openmim
+mim install mmdet
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu110/torch1.7.0/index.html
+pip install -r requirements/build.txt
+pip install -v -e .
+pip install -r requirements/albu.txt
