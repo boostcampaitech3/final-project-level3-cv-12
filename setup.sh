5187014d4dd78a5bcebd08f5f5fbc9b8e61274cb
ChoiHongrok <57215124+ChoiHongrok@users.noreply.github.com>
2022-04-14 23:15:07 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ conda install pytorch=1.7 cudatoolkit=11.0 torchvision=0.8.0 -c pytorch
 pip install openmim
 mim install mmdet
 pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu110/torch1.7.0/index.html
+cd mmdetection
 pip install -r requirements/build.txt
 pip install -v -e .
 pip install -r requirements/albu.txt
