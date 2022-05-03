cb8453218065838923262d30b715295e700a7ee6
ChoiHongrok <zxcv5317@naver.com>
2022-05-03 03:26:37 +0000

    apt install fontconfig

--- dataset/install_font.sh
+++ dataset/install_font.sh
@@ -3,6 +3,7 @@
 # 7 line 두번째 path 수정해야 정상작동함
 
 apt-get install fonts-nanum*
+apt install fontconfig -y
 fc-cache -fv
 cp /usr/share/fonts/truetype/nanum/Nanum* /opt/conda/envs/project/lib/python3.8/site-packages/matplotlib/mpl-data/fonts/ttf/
 rm -rf ~/.cache/matplotlib/*
