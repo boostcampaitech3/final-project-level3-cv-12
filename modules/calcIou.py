e089886e7ea8323ffeb5f32f2e74e154cb57ad8c
ChoiHongrok <zxcv5317@naver.com>
2022-05-29 12:51:55 +0000

    update

--- modules/calcIou.py
+++ modules/calcIou.py
@@ -1,44 +1,33 @@
 def checkIou(box1,box2):
-    box1_area = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
-    box2_area = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+    
+    x1, y1, w1, h1 = box1
+    x2, y2, w2, h2 = box2
+    x_max1, x_max2 = x1+w1, x2+w2
+    y_max1, y_max2 = y1+h1, y2+h2
 
-    x1 = max(box1[0], box2[0])
-    y1 = max(box1[1], box2[1])
-    x2 = min(box1[2], box2[2])
-    y2 = min(box1[3], box2[3])
+    box1_area = (w1 + 1) * (h1 + 1) # box1_area = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)# (w + 1) * (h + 1)
+    box2_area = (w2 + 1) * (h2 + 1) # box2_area = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)# (w + 1) * (h + 1)
+
+    x1_inter = max(x1, x2) # x1 = max(box1[0], box2[0])
+    y1_inter = max(y1, y2) # y1 = max(box1[1], box2[1])
+    x2_inter = min(x_max1, x_max2) # x2 = min(box1[2], box2[2])
+    y2_inter = min(y_max1, y_max2) # y2 = min(box1[3], box2[3])
 
     
-    w = max(0, x2 - x1 + 1)
-    h = max(0, y2 - y1 + 1)
+    w_inter = max(0, x2_inter - x1_inter + 1)
+    h_inter = max(0, y2_inter - y1_inter + 1)
 
-    inter = w * h
+    inter = w_inter * h_inter
     iou = inter / (box1_area + box2_area - inter)
     return iou
 
-def CalIou(all_box,change_box,size):
-    new_all_box , new_change_box = [],[]
 
-    for i in all_box:
-        xc,yc,w,h = float(i[1]) * size , float(i[2]) * size ,float(i[3]) * size , float(i[4]) * size
-        xmin = xc- w /2
-        ymin = yc - h /2
-        xmax = w + xmin
-        ymax = h + ymin
-        xmin , ymin , xmax, ymax = round(xmin,6),round(ymin,6),round(xmax,6),round(ymax,6)
-        new_all_box.append([i[0],xmin,ymin,xmax,ymax,i[5]])
-
-    for i in change_box:
-        xmin,ymin,xmax,ymax = i[0] , i[1] , i[0]+i[2] , i[1]+i[3]
-        new_change_box.append([xmin,ymin,xmax,ymax])
-
+def CalIou(all_box, change_box, threshold=0.8):
 
     out = []
-
-    for change_check in new_change_box:
-        for all_check in new_all_box:
-            c = [change_check[0],change_check[1],change_check[2],change_check[3]]
-            a = [all_check[1],all_check[2],all_check[3],all_check[4]]
-            if checkIou(c,a) > 0.8:
+    for change_check in change_box:
+        for all_check in all_box:
+            if checkIou(change_check, all_check) > threshold:
                 out.append(all_check)
             
     
