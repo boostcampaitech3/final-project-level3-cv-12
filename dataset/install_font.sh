9defb44801bb11f6b3d0838bb630063de24b58c9
ChoiHongrok <zxcv5317@naver.com>
2022-06-07 07:54:12 +0000

    rm

--- dataset/install_font.sh
+++
@@ -1,9 +0,0 @@
-# import matplotlib
-# matplotlib.__file__로 본인 matplotlib 경로 확인하고 
-# 7 line 두번째 path 수정해야 정상작동함
-
-apt-get install fonts-nanum*
-apt install fontconfig -y
-fc-cache -fv
-cp /usr/share/fonts/truetype/nanum/Nanum* /opt/conda/envs/project/lib/python3.8/site-packages/matplotlib/mpl-data/fonts/ttf/
-rm -rf ~/.cache/matplotlib/*
